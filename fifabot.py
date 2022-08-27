d1d308637f026f2fabd47750481662b134dcaa5c
Ricardo Simoes <ricardosimoess2001@gmail.com>
2022-08-27 17:53:03 +0000

    created dictionary for coordinate returning

--- fifabot.py
+++ fifabot.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from collections import *
 
 def initView(root):
 
@@ -8,53 +9,72 @@ def initView(root):
 
     searchLabel = Label(root, text="Search:").grid(row=1, column=0)
 
-    xsearchLabel = Entry(root).grid(row=1, column=1)
+    xsearchLabel : Entry = Entry(root).grid(row=1, column=1)
 
-    ysearchLabel = Entry(root).grid(row=1, column=2)
+    ysearchLabel : Entry  = Entry(root).grid(row=1, column=2)
 
     backLabel = Label(root, text="Back Button:").grid(row=2, column=0)
 
-    xbackLabel = Entry(root).grid(row=2, column=1)
+    xbackLabel : Entry  = Entry(root).grid(row=2, column=1)
 
-    ybackLabel = Entry(root).grid(row=2, column=2)
+    ybackLabel : Entry  = Entry(root).grid(row=2, column=2)
 
     buyNowLabel = Label(root, text="Buy Now Button:").grid(row=3, column=0)
 
-    xbuyNowLabel = Entry(root).grid(row=3, column=1)
+    xbuyNowLabel : Entry  = Entry(root).grid(row=3, column=1)
 
-    ybuyNowLabel = Entry(root).grid(row=3, column=2)
+    ybuyNowLabel : Entry  = Entry(root).grid(row=3, column=2)
 
     minBidPlusLabel = Label(root, text="Min Bid + Button:").grid(row=4, column=0)
 
-    xMinBidPlusLabel = Entry(root).grid(row=4, column=1)
+    xMinBidPlusLabel : Entry  = Entry(root).grid(row=4, column=1)
 
-    yMinBidPlusLabel = Entry(root).grid(row=4, column=2)
+    yMinBidPlusLabel : Entry  = Entry(root).grid(row=4, column=2)
 
     minBidMinusLabel = Label(root, text="Min Bid - Button:").grid(row=5, column=0)
 
-    xMinBidMinusLabel = Entry(root).grid(row=5, column=1)
+    xMinBidMinusLabel : Entry  = Entry(root).grid(row=5, column=1)
 
-    yMinBidMinusLabel = Entry(root).grid(row=5, column=2)
+    yMinBidMinusLabel : Entry  = Entry(root).grid(row=5, column=2)
 
     minBNPlusLabel = Label(root, text="Min Buy Now + Button:").grid(row=6, column=0)
 
-    xMinBNPLabel = Entry(root).grid(row=6, column=1)
+    xMinBNPLabel : Entry  = Entry(root).grid(row=6, column=1)
 
-    yMinBNPLabel = Entry(root).grid(row=6, column=2)
+    yMinBNPLabel : Entry  = Entry(root).grid(row=6, column=2)
 
     minBNMinusLabel = Label(root, text="Min Buy Now - Button:").grid(row=7, column=0)
 
-    xminBNMinusLabel = Entry(root).grid(row=7, column=1)
+    xminBNMinusLabel : Entry  = Entry(root).grid(row=7, column=1)
 
-    yminBNMinusLabel = Entry(root).grid(row=7, column=2)
+    yminBNMinusLabel : Entry  = Entry(root).grid(row=7, column=2)
 
     submitButton = Button(root, text="Submit !").grid(row=8, column= 1)
 
+    coordinates = {
+        "xsearchLabel" : xsearchLabel.get(),
+        "ysearchLabel" : ysearchLabel.get(),
+        "xbackLabel" : xbackLabel.get(),
+        "ybackLabel" : ybackLabel.get(),
+        "xbuyNowLabel" : xbuyNowLabel.get(),
+        "ybuyNowLabel" : ybuyNowLabel.get(),
+        "xMinBidPlusLabel" : xMinBidPlusLabel.get(),
+        "yMinBidPlusLabel" : yMinBidPlusLabel.get(),
+        "xMinBidMinusLabel" : xMinBidMinusLabel.get(),
+        "yMinBidMinusLabel" : yMinBidMinusLabel.get(),
+        "xMinBNPLabel" : xMinBNPLabel.get(),
+        "yMinBNPLabel" : yMinBNPLabel.get(),
+        "xminBNMinusLabel" : xminBNMinusLabel.get(),
+        "yminBNMinusLabel" : yminBNMinusLabel.get()
+    }
 
+    return coordinates
 
 root = Tk()
 
-initView(root)
+buttonCoordinates = initView(root)
+
+print(buttonCoordinates)
 
 
 
